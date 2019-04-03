d5a0783f2c02d22dd1b6ddc2d1713223e2fb87c9
pigyeoun <pigyeoun@gmail.com>
2019-04-03 09:38:13 +0900

    01

--- Helloworld/src/Helloworld.java
+++ Helloworld/src/Helloworld.java
@@ -2,10 +2,6 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("World's End Danceha11");
-<<<<<<< HEAD
-		System.out.println("Earth");
-=======
 		System.out.println("Hello");
->>>>>>> refs/heads/hotfix
 	}
 }
---
+++ Helloworld/src/pushtest.java
@@ -0,0 +1,5 @@
+public class pushtest {
+	public static void main(String[] args) {
+		System.out.println("Add Java Class File Push");
+	}
+}
