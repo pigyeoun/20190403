486bc02272431f7633a2139093d48e2072d5d6d7
pigyeoun <48500680+pigyeoun@users.noreply.github.com>
2019-04-03 10:44:58 +0900

    Update pushtest.java

--- Helloworld/src/pushtest.java
+++ Helloworld/src/pushtest.java
@@ -3,5 +3,6 @@ public class pushtest {
 		System.out.println("Add Java Class File Push");
 		System.out.println("World with 50");
 		System.out.println("World with 42");
+		System.out.println("World with 502");
 	}
 }
